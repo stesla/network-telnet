4258bcfd7542e37f833d2ec8e3ee46d6d6e535b0
Samuel Tesla <samuel.tesla@gmail.com>
2014-06-20 17:05:26 -0500

    get more parsers written

--- src/Network/Protocol/Telnet.hs
+++ src/Network/Protocol/Telnet.hs
@@ -14,6 +14,7 @@ data Chunk = Bytes B.ByteString
            deriving (Eq, Ord, Show)
 
 data Command = Command Word8
+             | LiteralIAC
              | WILL Option
              | WONT Option
              | DO Option
@@ -23,6 +24,17 @@ data Command = Command Word8
 newtype Option = Option Word8
                deriving (Eq, Ord, Show)
 
+iac = word8 255
+
+chunk = command <|> bytes
+
+bytes = takeWhile1 (/=255) >>= pure . Bytes
+
+command = word8 255 >> do
+  willOption <|> wontOption <|> doOption <|> dontOption <|> literalIAC <|> command'
+  where command' = anyWord8 >>= pure . IAC . Command
+
+literalIAC = word8 255 >> pure (IAC LiteralIAC)
 willOption = word8 251 >> anyWord8 >>= pure . IAC . WILL . Option
 wontOption = word8 252 >> anyWord8 >>= pure . IAC . WONT . Option
 doOption   = word8 253 >> anyWord8 >>= pure . IAC . DO   . Option
