1490b8b9ef65d5fa5b1d5d34f578fd2d7ae7a06f
Samuel Tesla <samuel.tesla@gmail.com>
2014-06-23 14:11:33 -0500

    fix readChunk to better handle empty input

--- src/Network/Protocol/Telnet.hs
+++ src/Network/Protocol/Telnet.hs
@@ -24,14 +24,17 @@ newtype Option = Option Word8
 type ParseResult = Maybe (Chunk, B.ByteString)
 
 readChunk :: B.ByteString -> ParseResult
-readChunk str = case B.elemIndex 255 str of
-  Nothing -> Just (Bytes str, B.empty)
-  Just 0  -> readCommand $ B.tail str
-  Just n  ->
-    let (xs, str') = B.splitAt n str in
-    case readCommand $ B.tail str' of
-      Just (Bytes ys, str'') -> return (Bytes $ B.append xs ys, str'')
-      _                      -> return (Bytes xs, str')
+readChunk str = if B.length str == 0 then Nothing else readChunk'
+  where
+    readChunk' =
+      case B.elemIndex 255 str of
+        Nothing -> Just (Bytes str, B.empty)
+        Just 0  -> readCommand $ B.tail str
+        Just n  ->
+          let (xs, str') = B.splitAt n str in
+          case readCommand $ B.tail str' of
+            Just (Bytes ys, str'') -> return (Bytes $ B.append xs ys, str'')
+            _                      -> return (Bytes xs, str')
 
 readCommand :: B.ByteString -> ParseResult
 readCommand str = do
